1d212c1bd646601cbc487d7e7870e86e3206844f
BenCrystalMLB <115038165+BenCrystalMLB@users.noreply.github.com>
2023-04-13 16:56:59 -0400

    Added hand prefabs to ball in SDK Pose Tool

---
+++ Assets/ReturnToSpawn.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToSpawn : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public int updateInterval = 1;
+    public float minY = 0;
+
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = this.transform.position;
+
+        rb = GetComponent<Rigidbody>();
+
+        StartCoroutine(CheckPositionAndReset());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(CheckPositionAndReset());
+    }
+
+
+
+    private IEnumerator CheckPositionAndReset()
+    {
+        while (true)
+        {
+            if (transform.position.y < minY)
+            {
+                transform.position = initialPosition;
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+
+            yield return new WaitForSeconds(updateInterval);
+        }
+    }
+
+
+}
