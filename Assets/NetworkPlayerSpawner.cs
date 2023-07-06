db465917c5ed32a5fcc5970324b254b8ef111a98
BenCrystalMLB <115038165+BenCrystalMLB@users.noreply.github.com>
2023-07-06 15:59:33 -0400

    Relatively post Valem tutorial 1
    Added a build with testplayer but may need to be reworked

--- Assets/NetworkPlayerSpawner.cs
+++ Assets/NetworkPlayerSpawner.cs
@@ -5,10 +5,25 @@ using Photon.Pun;
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
+
+    //private PhotonView photonView = spawnedpla
+    //private GameObject originalPlayer; //used for referencing head and hand locations
+    /*void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }*/
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("TestPlayer", transform.position, transform.rotation);
+
+        //if (!photonView.IsMine)
+        {
+            spawnedPlayerPrefab = PhotonNetwork.Instantiate("TestPlayer", transform.position, transform.rotation);
+        }
+        //spawnedPlayerPrefab = PhotonNetwork.Instantiate("TestPlayer", transform.position, transform.rotation);
+        //spawns
+
+        //spawnedPlayerPrefab.GetComponent<>.head = originalPlayer.GetComponent<GameObject>
         //spawnedPlayerPrefab = PhotonNetwork.Instantiate("OVRPHOTONTESTRIG", transform.position, transform.rotation);
     }
 
--- Assets/TestPlayer.cs
+++ Assets/TestPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.XR;
 using Oculus.Interaction;
 //using UnityEngine.XR.
+using Photon.Pun;
 
 
 /*
@@ -17,29 +18,57 @@ using Oculus.Interaction;
 public class TestPlayer : MonoBehaviour
 {
     public Transform head;
-    public Transform leftGlove;
+    public Transform leftHand;
     public Transform rightHand;
+
+    public Transform testHead;
+    public Transform testLeftGlove;
+    public Transform testRightHand;
+
+    private PhotonView photonView;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        photonView = GetComponent<PhotonView>();
+
+        head = GameObject.Find("OVRCameraRig").transform.Find("TrackingSpace").Find("CenterEyeAnchor").transform;
+        leftHand = GameObject.Find("OVRCameraRig").transform.Find("TrackingSpace").Find("LeftHandAnchor").transform;
+        rightHand = GameObject.Find("OVRCameraRig").transform.Find("TrackingSpace").Find("RightHandAnchor").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MapPosition(head, XRNode.Head);
-        MapPosition(leftGlove, XRNode.LeftHand);
-        MapPosition(rightHand, XRNode.RightHand);
+        if(photonView.IsMine)
+        {
+            testHead.gameObject.SetActive(false);
+            testLeftGlove.gameObject.SetActive(false);
+            testRightHand.gameObject.SetActive(false);
+
+            MapPosition(testHead, head);
+            MapPosition(testLeftGlove, leftHand);
+            MapPosition(testRightHand, rightHand);
+        }
+
+        //MapPosition(testHead, XRNode.Head);
+        
+        
+        //MapPosition(testLeftGlove, XRNode.LeftHand);
+        //MapPosition(testRightHand, XRNode.RightHand);
+        
         //MapPosition(leftGlove, XRNode.LeftHand);
         //MapPosition(rightHand, XRNode.RightHand);
     }
 
-    void MapPosition(Transform target, XRNode node)
+    //void MapPosition(Transform target, XRNode node)
+    void MapPosition(Transform target, Transform origin)
     {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+        Vector3 position = origin.transform.position;
+        Quaternion rotation = origin.transform.rotation;
+
+        //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
 
         target.position = position;
         target.rotation = rotation;
